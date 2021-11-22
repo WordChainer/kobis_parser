a249c33af464186c4e66755671b9a7498769a42e
xCrypt0r <legacy5877@gmail.com>
2021-11-23 03:44:32 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+﻿const got = require('got');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const URL = 'https://www.kobis.or.kr/kobis/business/mast/mvie/searchMovieList.do';
+
+async function main() {
+    let words = [],
+        promises = [];
+
+    for (let i = 8001; i <= 8470; i++) {
+        promises.push(getPage(words, i));
+    }
+
+    Promise
+        .all(promises)
+        .then(() => {
+            console.log('파싱 완료');
+            fs.writeFileSync('result.txt', words.join('\r\n'));
+        });
+}
+
+async function getPage(words, page) {
+    let { body } = await got.post(URL, {
+        form: { curPage: page },
+        retry: { limit: 10, methods: ['POST'] },
+        hooks: {
+            beforeRetry: [
+                (error, retryCount) => {
+                    console.log(`${page}페이지 파싱 실패 ${retryCount}번째 재시도`);
+                }
+            ]
+        }
+    });
+    let $ = cheerio.load(body);
+
+    $('tbody tr td:first-child').each((i, el) => words.push($(el).text().trim()));
+    console.log(`누적: ${words.length}`);
+}
+
+main();
